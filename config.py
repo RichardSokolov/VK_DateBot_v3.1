77dca4db5c720193ca26c1609581763a9d74606a
RichardSokolov <101970011+RichardSokolov@users.noreply.github.com>
2023-07-03 07:20:33 +0300

    new

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-user_token = 'vk1.a.dnoDNvnexlkTM6XC5YfLAPVsObDP16tzRCwxwASasvkD28N10cMN_gu3SmreiLLlIN-jUCsYTKusZ5hPSuUDv7zFcOi_oVtBP4myBjwvKo2BRD9QXUxvFwZsrcdwlrYM4MhvoR-4qG_v2b0PIQBWYywB0-Rj8iXB917b_ryn3ulJtr2k2Pje-22z9vSfcHTLoLhyqb_3GUgbRU29vfTxYQ' #токен пользователя
-group_token = 'vk1.a.LA7HJujujT5dcihDE-3YnXnF6AjyXas6wiNHLcM-ihFC7XEzp_WXixQNIj5Kq8k_ZDeFBCn9t5cwloDaskFe2WbKol9_DfzywlFE7nDwcdaAOGHe75OfBI61wOLD8GrqpclXWEL6rva68zK_irBGopOiSNs-IbIM8lpszxAA-ZWpp4Y_WY5MA2P5NKPKLqryLkFGHeYnizti9_Vnne-WFw' #токен группы
+user_token = '***' #токен пользователя
+group_token = '***' #токен группы
 
 line = range(0, 1000)
-config_data = ('127.0.0.1', 'postgres', '14032001', 'bot_users')
+config_data = ('127.0.0.1', 'postgres', '***', 'bot_users')
                             #имя пользователя, #пароль, #название бд
 count = 10
 offset = 0
